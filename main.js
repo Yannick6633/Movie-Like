5a702fa828a90e8bb6e84cd6817055ebae11698b
Yannick6633 <46927977+Yannick6633@users.noreply.github.com>
2020-10-15 16:23:58 +0200

    Fetch images

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
+let title = document.querySelector('.list #titleMovie');
+let poster_path = document.querySelector('.list');
+
 function movieResults () {
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=c174da4ce913549d5180cc8cc9456b00')
     .then(dataMovie => dataMovie.json())
     .then(dataMovie => {
-        console.log(dataMovie);
+        //console.log(dataMovie);
         displayResults(dataMovie.results);
     });
 }
 
 function displayResults (dataMovie) {
-    let title = document.querySelector('.list');
-    let affichage = ``;
-    dataMovie.forEach(movie => {
-        affichage += `<li>movie.title</li>`
-    });
-    title.innerHTML = affichage;
-}
-//movieResults();
 
-        
+        let affichage = ``;
+        dataMovie.forEach(movie => {
+            affichage += movie.title + '<br>';
+        });
+        title.innerHTML = affichage;
+
+        let aff = ``;
+        dataMovie.forEach(movie => {
+            aff += `<img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">`;
+        });
+        poster_path.innerHTML = aff;
+
+}
+movieResults();
+
+         
